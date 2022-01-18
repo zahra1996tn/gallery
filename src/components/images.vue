4368a86e651b0fa2745aceea77e1df6d4a2bc1f9
zahra <z.tanhaei1996@gmail.com>
2022-01-18 11:41:51 +0330

    change file

---
+++ src/components/images.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+      <!-- <img v-for="im in image" :key="im" :src="im" class="img"> -->
+      <!-- <a> <i class="fa fa-heart"  v-on="$Listener"></i> {{a}} </a> -->
+      <img :src="img" class="img">
+      <like  @click="like" :a="a"/>
+  </div>
+</template>
+
+<script>
+import like from './like.vue'
+
+export default {
+  components: { like },
+    name:'images',
+    // components:{like },
+    props:['img'],
+    data(){
+        return{
+            a:0,
+            // image:[
+            //     '../assets/3.jpg',
+            //     '../assets/4.jpg',
+            //     '../assets/2.jpg'
+            // ]
+        }
+    
+    },
+     methods:{
+        like(){
+            this.a ++;
+            // this.color='red';
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    .img{
+        width: 280px;
+        height: 200px;
+        margin: 20px 10px;
+        border-radius: 6px;
+        cursor: pointer;
+    }
+    .box-img{
+        width: 280px;
+        height: 200px;
+    }
+</style>
---
+++ src/components/like.vue
@@ -0,0 +1,18 @@
+<template>
+      <a> <i class="fa fa-thumbs-up"  v-on="$Listener"></i>  {{ a }}</a>
+</template>
+
+<script>
+export default {
+    props:['a'],
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
