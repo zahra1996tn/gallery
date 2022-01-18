eeffe2d5bfcf6b27fcb8d873c25e18dd5a3e08c5
zahra <z.tanhaei1996@gmail.com>
2022-01-18 11:35:56 +0330

    change file

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,26 @@
-<template>
-<div>
-  <img class="img-bg" src="./assets/template.jpg">
+<template> 
+ <h1> گالری تصاویر </h1>
+ <h2> در دنیایی که همه چیز خیلی زود فراموش می‌شود،
+عکس‌ها سندی هستند که با ثبت یک لحظه،
+راوی تاریخ می‌شوند. </h2>
+<div class="bg">
+  <img src='./assets/36.jpg' class="img-bg">
 </div>
-<div class="box-bold">
-  <img src="./assets/2.jpg">
-  <img src="./assets/4.jpg">
-  <img src="./assets/5.jpg">
-  <img src="./assets/6.jpg">
-  <img src="./assets/7.jpg">
-</div>
-<div class="container">
-  <gallery :src="img" v-for="img in images" :key="img" class="" />
-</div>
+<gallery />
+
 </template>
 
 <script>
 import gallery from './components/gallery.vue'
 
 export default {
-  components: { gallery },
+  components: { gallery},
   name: 'App',
   data(){
     return{
-        images:[
-        "https://s4.uupload.ir/files/3_0jxd.jpg",
-        "https://s4.uupload.ir/files/8_blfi.jpg",
-        "https://s4.uupload.ir/files/7_5u2y.jpg",
-        'https://s4.uupload.ir/files/11_y993.jpg',
-        'https://s4.uupload.ir/files/5_7kmw.jpg',
-        'https://s4.uupload.ir/files/2_6j5.jpg',
-
-        ],
     }
-  }
+  },
+
 }
 </script>
 
@@ -43,65 +31,40 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   text-align: center;
+  font-family: IS;
 }
+@font-face{
+font-family: IS;
+font-style: normal;
+src: url("../public/font/IRAN Sans Regular (mmrostami.blog.ir).ttf");
+}
+h1{
+  position: absolute;
+    margin: 200px 276px;
+    font-size: 41px;
+    color: #2c3e50;
+}
+h2{
+  position: absolute;
+    margin: 280px 200px;
+    font-size: 20px;
+    max-width: 300px;
+        text-align: right;
+    word-spacing: 11px;
+}
+/* @media(max-width:587px){
+  h1{
+    margin: 290px 120px;
+  }
+ 
+} */
 .container{
-  position: relative;
-  margin-top: 140px;
-  z-index: 555;
-  display: inline-block;
   justify-content: center;
-  width: 900px;
   text-align: center;
-  /*shadow*/ 
-  border-radius: 15px;
-  background: #f5f5f5;
-  box-shadow: -2px 14px 30px #bebebe, 2px 2px 89px #ffffff;
+  display: inline-grid;
 }
-@media(max-width:925px){
-  .container{
-    width: 100%;
-    justify-content: center;
-    display: inline-block;
-    text-align: center;
-    align-items: center;
-    margin: 55px auto;
-  }
-}
-.img-bg{
-  width: 100%;
-}
-.box-bold img{
-  float: right;
-  width: 70px;
-  height: 70px;
-  margin-right: 66px;
-}
-.box-bold{
-  padding: 13px;
-  width: 700px;
-  height: 70px;
-  justify-content: center;
-  display: inline-flex;
-  margin: -34px auto;
-  border-radius: 4px;
-  background: #f5f5f5;
-  box-shadow: -9px 4px 50px #bebebe, 1px 16px 13px #ffffff;
-  }
-  @media(max-width:800px){
-    .box-bold{
-      width: 79%;
-      margin:-60px auto ;
 
-    }
-    .box-bold img{
-      margin-right: 23px;
-    }
-  }
-  @media(max-width:615px){
-    .box-bold{
-      display: none;
-    }
-  }
+
   @media(max-width:330px){
     .container img{
       width: 210px !important;
@@ -114,4 +77,107 @@ export default {
       border: none !important;
     }
   }
-</style>
+  .bg{
+    height: 600px;
+    background-color: #fff;
+  }
+  .img-bg{
+    margin-left: 600px;
+  }
+  @media(max-width:1224px){
+    .img-bg{
+      margin-left: 550px;
+    }
+    h1{
+      margin: 216px 276px;
+    }
+  }
+   @media(max-width:1175px){
+    .img-bg{
+      margin-left: 400px;
+      width: 500px;
+    }
+   
+  } 
+  @media(max-width:1100px){
+    h1{
+      margin: 216px 200px;
+    }
+   h2{
+     margin: 280px 100px;
+   }
+  }
+  @media(max-width:925px){
+    .img-bg{
+      margin-left: 320px;
+      width: 430px;
+    }
+    h1{
+    margin: 180px 70px;
+    }
+    h2{
+    margin: 250px 20px;
+    }
+    .bg{
+      height: 450px;
+    }
+  }
+  @media(max-width:775px){
+    .img-bg{
+      margin-left: 250px;
+      width: 380px;
+    }
+  }
+  @media(max-width:655px){
+    .img-bg{
+      margin-left: 200px;
+      width: 320px;
+    }
+    h1{
+      margin: 120px 50px;
+      font-size:35px ;
+    }
+    h2{
+    margin: 190px 10px;
+    }
+    .bg{
+      height: 400px;
+    }
+  } 
+   @media(max-width:545px){
+    .img-bg{
+      margin-left: 150px;
+      width: 300px;
+    }
+    h1{
+      margin: 120px 30px;
+      font-size:32px ;
+    }
+    h2{
+      margin: 190px 0px;
+      font-size:19px ;
+    }
+  }
+  @media(max-width:475px){
+    .img-bg{
+      margin: 0 5px;
+      width: 250px;
+    }
+    h1{
+      display: block;
+      /* margin: 320px 45px; */
+      font-size:32px ;
+      justify-content: center;
+      align-items: center;
+      margin-top: 280px;
+    }
+    h2{
+      margin-top:340px ;
+      max-width:250px ;
+      text-align: justify !important;
+    }
+    .bg{
+      height: 500px;
+    }
+  }
+  </style>
--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,31 +1,41 @@
 <template>
-<div class="img">
+<!-- <div class="img">
     <img :src="src" :style="styleimg" />
     <a> <i class="fa fa-heart-o" :style="{color:color }" @click="like"></i>  {{ a }}
  </a>
-</div>
+</div> -->
+<images img="https://s4.uupload.ir/files/21_3flo.jpg" />
+<images img="https://s4.uupload.ir/files/13_2ews.jpg" />
+<images img="https://s4.uupload.ir/files/15_kq3n.jpg" />
+<images img="https://s4.uupload.ir/files/24_aqxj.png" />
+<images img="https://s4.uupload.ir/files/27_e9iy.jpg" />
+<images img="https://s4.uupload.ir/files/18_9e7g.jpg" />
+<images img="https://s4.uupload.ir/files/31_5kw.jpg" />
+<images img="https://s4.uupload.ir/files/26_10gh.jpg" />
+<images img="https://s4.uupload.ir/files/a12235132475731.61c5183fe2aeb_m90l.jpg" />
+<images img="https://s4.uupload.ir/files/66bafa127490591.y3jvccwznjczldi4nzismtyzldu2mw_0ip9.jpg" />
+<images img="https://s4.uupload.ir/files/23c991132019215.y3jvccwxntm0ldeymdasndesma_m6f.png" />
+<images img="https://s4.uupload.ir/files/2_1gmr.jpg" />
 </template>
 
 <script>
+import images from './images.vue'
+
 export default {
+    components:{images},
     name:'gallery',
-    props:['src'],
     data(){
         return{
-           a:0,
+             a:0,
            styleimg:{
                width:'250px',
                height:'210px',
                marginTop:'-32px'
-           }
+           },
+           src:['./assets/2.jpg']
         }
     },
-    methods:{
-        like(){
-            this.a ++;
-            this.color='red';
-        }
-    }
+
 }
 </script>
 
@@ -33,14 +43,6 @@ export default {
 
 body{
     background:#eee;
-}
-.img{
-    float: right;
-    margin: 10px 23px;
-    padding: 32px 0;
-    height: 191px;
-    border: 1px solid rgb(218, 218, 218);
-    border-radius: 5px;
 }
 .img-head{
     margin: 10px;
@@ -50,7 +52,9 @@ i{
     cursor: pointer;
 }
 a{
-    display: block !important;
-    margin-top: 15px;
+    /* display: block !important; */
+    margin-top: -13px;
+    margin-left: 212px;
+    font-size: 15px;
 }
 </style>
